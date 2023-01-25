58f568678d0cdd33fec579555a72a769674d48cd
Mahamodul Hasan Moon <coddermoon@gmail.com>
2023-01-26 02:34:07 +0600

    done navbar

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,48 +1,43 @@
-import React from 'react';
-import { AiOutlineQuestionCircle, AiOutlineUser } from 'react-icons/ai';
-import { CiSearch } from 'react-icons/ci';
-import { FiSettings } from 'react-icons/fi';
+import React from "react";
+import { AiOutlineQuestionCircle, AiOutlineUser } from "react-icons/ai";
+import { CiSearch } from "react-icons/ci";
+import { FiSettings } from "react-icons/fi";
+import { navItems } from "../utils/navitems";
 
 const MobileNav = () => {
-    return (
-        <div>
-                  <div className="flex items-center justify-between p-4">
-          <div className="flex items-center">
-            {/* <img src="logo.png" alt="Logo" className="h-8" /> */}
-            <span className="ml-2 text-lg font-medium">solver</span>
-          </div>
-         
-            <a
-              to=""
-              className="flex gap-2 items-center px-3 py-2 rounded-md text-sm font-medium leading-5  focus:outline-none focus:text-white focus:bg-gray-700"
-            >
-              <AiOutlineQuestionCircle />
-             
-            </a>
-            <a
-              to=""
-              className="flex gap-2 items-center px-3 py-2 rounded-md text-sm font-medium leading-5  focus:outline-none focus:text-white focus:bg-gray-700"
-            >
-              <CiSearch /> 
-            </a>
-            <a
-              to=""
-              className="flex gap-2 items-center px-3 py-2 rounded-md text-sm font-medium leading-5  focus:outline-none focus:text-white focus:bg-gray-700"
-            >
-              <AiOutlineUser /> 
-            </a>
-          
-          <div>
-            <a
-              href="#"
-              className="flex gap-2 items-center px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 focus:outline-none focus:text-white focus:bg-gray-700"
-            >
-              <FiSettings /> 
-            </a>
-          </div>
-        </div>  
+  return (
+    <div>
+      <div className="flex items-center justify-between p-4">
+        <div className="flex items-center">
+          {/* <img src="logo.png" alt="Logo" className="h-8" /> */}
+          <span className="ml-2 text-lg font-medium">solver</span>
         </div>
-    );
+
+
+
+        {
+    navItems.map((navItem, index) =>             <a key={index}
+    to=""
+    className="flex gap-2 items-center px-3 py-2 rounded-md text-sm font-medium leading-5  focus:outline-none focus:text-white focus:bg-gray-700"
+  >
+    <navItem.icons />
+    <span className="hidden md:block">{navItem.name}</span>
+  </a>
+    )
+}
+  
+
+        <div>
+          <a
+            href="#"
+            className="flex gap-2 items-center px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 focus:outline-none focus:text-white focus:bg-gray-700"
+          >
+            <FiSettings />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MobileNav;
