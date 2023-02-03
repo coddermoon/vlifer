303d350f6299343c94b89ad6ac990d1b6ac673b8
Mahamodul Hasan Moon <coddermoon@gmail.com>
2023-02-04 01:21:37 +0600

    vlifer basic navbar done

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../pages/shared/Navbar/Navbar';
+import VliferNav from '../pages/shared/Navbar/VliferNav';
 
 const Main = () => {
     return (
         <div className="bg-bgColor min-h-screen text-textColor">
-        <Navbar/>
+        <VliferNav/>
       <Outlet/>
             </div>
     );
--- src/pages/shared/Navbar/VliferNav.js
+++ src/pages/shared/Navbar/VliferNav.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import logo from "../../../assets/images/logo.svg";
+import { NavLink } from "react-router-dom";
+import { activeClassName, normalClassName } from "../../../utils/navitems";
 
 const VliferNav = () => {
-    return (
-        <div>
-            
+  return (
+    <nav  className="bg-white py-1 "  style={{boxShadow:'0px 1px 2px rgba(0, 0, 0, 0.25)'}}>
+<div className="container mx-auto">
+<div className="flex items-center justify-between px-4">
+        <div className="flex items-center justify-center">
+          <img src={logo} alt="Logo" className="h-8" />
+          <span className="ml-2  text-xl font-medium text-primaryColor">
+            Solver
+          </span>
         </div>
-    );
+      
+<div className="flex">
+<NavLink  to="/about" className={({ isActive }) =>isActive ? normalClassName + " font-semibold" : normalClassName
+            }
+          >
+             About
+          </NavLink>
+
+          <NavLink  to="/tools" className={({ isActive }) =>isActive ? normalClassName + " font-semibold" : normalClassName
+            }
+          >
+           Tools
+          </NavLink>
+          <NavLink  to="/contacts" className={({ isActive }) =>isActive ? normalClassName + " font-semibold" : normalClassName
+            }
+          >
+        Contact
+          </NavLink>
+
+          <NavLink  to="/login" className={({ isActive }) =>isActive ? activeClassName + " font-semibold rounded" : activeClassName+ ' rounded'
+            }
+          >
+        Enter
+          </NavLink>
+</div>
+    
+      </div>
+</div>
+    </nav>
+  );
 };
 
 export default VliferNav;
