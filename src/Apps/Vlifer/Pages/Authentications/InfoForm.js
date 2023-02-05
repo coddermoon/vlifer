fa7e950e2ddded8adf6bce74acb902517ff3bc87
Mahamodul Hasan Moon <coddermoon@gmail.com>
2023-02-06 00:44:16 +0600

    page no 3 done

---
+++ src/Apps/Vlifer/Pages/Authentications/InfoForm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const InfoForm = () => {
+    return (
+        <div className='my-8 py-20 custom-shadow flex items-center'>
+          <div className="min-w-[340px] mx-auto">
+           <p className="text-center text-xl font-semibold text-textColor">Fill this form for Solver perfect browsing .</p>
+
+           <form className='mt-16 '>
+
+<div className='grid grid-cols-2 gap-2'>
+<div className=' '>
+<label htmlFor="NewPassword">Full Name</label> <br />
+<input className='mt-1 w-full py-1 borderInputCustom rounded px-2 outline-none' name='newPassword' type="text" placeholder='Full Name'  />
+</div>
+<div className=''>
+<label htmlFor="NewPassword">Nick name</label> <br />
+<input className='mt-1 w-full py-1 borderInputCustom rounded px-2 outline-none' name='newPassword' type="text" placeholder='Nick name'  />
+</div>
+
+<div className=''>
+<label htmlFor="NewPassword">Age</label> <br />
+<input className='mt-1  w-full  py-1 borderInputCustom rounded px-2 outline-none' name='newPassword' type="text" placeholder='Age'  />
+</div>
+
+<div className=''>
+<label htmlFor="NewPassword">Gender</label> <br />
+<input className='mt-1  w-full  py-1 borderInputCustom rounded px-2 outline-none' name='newPassword' type="text" placeholder='Gender'  />
+</div>
+
+<div className=''>
+<label htmlFor="NewPassword">Occupation</label> <br />
+<input className='mt-1  w-full  py-1 borderInputCustom rounded px-2 outline-none' name='newPassword' type="text" placeholder='Occupation'  />
+</div>
+
+<div className=''>
+<label htmlFor="NewPassword">Riligion</label> <br />
+<input className='mt-1  w-full  py-1 borderInputCustom rounded px-2 outline-none' name='newPassword' type="text" placeholder='Riligion'  />
+</div>
+
+<div className=''>
+<label htmlFor="NewPassword">Language</label> <br />
+<input className='mt-1  w-full  py-1 borderInputCustom rounded px-2 outline-none' name='newPassword' type="text" placeholder='Language'  />
+</div>
+
+<div className=''>
+<label htmlFor="NewPassword">Nationality</label> <br />
+<input className='mt-1  w-full  py-1 borderInputCustom rounded px-2 outline-none' name='newPassword' type="text" placeholder='Nationality'  />
+</div>
+
+
+</div>
+
+
+<div className="btn text-center mt-16">
+<input className='bg-primaryColor px-4 rounded py-2 text-center text-white font-semibold ' type="submit" />
+</div>
+           </form>
+
+          </div>
+        </div>
+    );
+};
+
+export default InfoForm;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import ChangePass from "../Apps/Vlifer/Pages/Authentications/ChangePass";
 import Forgotten from "../Apps/Vlifer/Pages/Authentications/Forgotten";
+import InfoForm from "../Apps/Vlifer/Pages/Authentications/InfoForm";
 import AuthenticationLayout from "../Layout/AuthenticationLayout";
 
 export const mainRoutes = createBrowserRouter([
@@ -14,6 +15,10 @@ export const mainRoutes = createBrowserRouter([
         },{
             path:'/change-password',
             element:<ChangePass/>,
+        }
+        ,{
+            path:'/personal-information',
+            element:<InfoForm/>,
         }
     ]
 }
