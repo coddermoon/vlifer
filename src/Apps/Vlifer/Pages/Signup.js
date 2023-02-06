27555eb909c971207060bd3c0398a3ec45d624ab
Mahamodul Hasan Moon <coddermoon@gmail.com>
2023-02-06 23:01:49 +0600

    done login and sign up

--- src/Apps/Vlifer/Pages/Signup.js
+++ src/Apps/Vlifer/Pages/Signup.js
@@ -20,17 +20,23 @@ const Signup = () => {
 <input className='mt-1 w-full py-1 borderInputCustom rounded px-2 outline-none' name='email' type="text"  />
 </div>
 <div className='my-3'>
-<label htmlFor="Password">Password</label> <br />
-<input className='mt-1  w-full  py-1 borderInputCustom rounded px-2 outline-none' name='Password' type="password"   />
+<label htmlFor="newPassword">New Password</label> <br />
+<input className='mt-1  w-full  py-1 borderInputCustom rounded px-2 outline-none' name='newPassword' type="password"   />
+</div>
+
+<div className='my-3'>
+<label htmlFor="newPassword">Confirm Password</label> <br />
+<input className='mt-1  w-full  py-1 borderInputCustom rounded px-2 outline-none' name='confirmPassword' type="password"   />
 </div>
 
 
-<div className="btn flex items-center justify-between mt-16">
-    <Link className='text-primaryColor text-xl'>Forgot password</Link>
-<button className='bg-primaryColor px-4 rounded py-2 text-center text-white font-semibold ' type="submit">Login</button>
+<div className="btn text-center mt-14">
+<input className='bg-primaryColor px-4 rounded py-2 text-center text-white font-semibold ' type="submit" />
 </div>
+
+
 <div className='flex bg-bgColor items-center mt-10'>
-<Link className='text-primaryColor mx-auto text-xl  text-center'>Register Your ID</Link>
+<button className='text-primaryColor mx-auto text-xl  text-center'>Register With Google</button>
 </div>
 
 
